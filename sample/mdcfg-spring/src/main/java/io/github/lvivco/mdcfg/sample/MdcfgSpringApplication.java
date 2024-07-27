c864e13c48c54969663dd2f97a2f43233f93e2f9
vstyran <vstyran@softserveinc.com>
2024-07-27 20:52:17 +0300

    Create simple Spring integration example #57

---
+++ sample/mdcfg-spring/src/main/java/io/github/lvivco/mdcfg/sample/MdcfgSpringApplication.java
@@ -0,0 +1,13 @@
+package io.github.lvivco.mdcfg.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdcfgSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdcfgSpringApplication.class, args);
+	}
+
+}
